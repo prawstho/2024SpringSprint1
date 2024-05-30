d7290e57a63398ec7f6aca5104431b35a19f57f0
Peter <113168584+prawstho@users.noreply.github.com>
2024-05-30 10:32:46 -0230

    added init features

--- init.js
+++ init.js
@@ -1,6 +1,63 @@
 const fs = require('fs');
+const fsPromises = require('fs').promises;
+const path = require('path');
 
 const myArgs = process.argv.slice(2);
+
+const folders = ['models', 'views', 'routes', 'logs', 'json'];
+
+const configjson = { 
+    name: 'AppConfigCLI',
+    version: '1.0.0',
+    description: 'The Command Line Interface (CLI) for the MyApp.',
+    main: 'myapp.js',
+    superuser: 'adm1n',
+    database: 'exampledb'
+  };
+
+function createFiles() {
+    if(DEBUG) console.log('init.createFiles()');
+    try {
+        let configdata = JSON.stringify(configjson, null, 2);
+        if(!fs.existsSync(path.join(__dirname, './json/config.json'))) {
+            fs.writeFile('./json/config.json', configdata, (err) => {
+                if(err) {
+                    console.log(err)
+                }
+                else {
+                    console.log('Data written to config file.');
+                }
+            })
+        } else {
+          console.log('config file already exists.');
+        }
+    } catch(err) {
+        console.log(err);
+    }
+  };
+
+function createFolders() {
+    if(DEBUG) console.log('init.createFolders()');
+    let mkcount = 0;
+    folders.forEach(folder => {
+        if(DEBUG) console.log(folder);
+        try {
+            if(!fs.existsSync(path.join(__dirname, folder))) {
+                fsPromises.mkdir(path.join(__dirname, folder));
+                mkcount++;
+            }
+        } catch (err) {
+            console.log(err);
+        }
+    });
+    if(mkcount === 0) {
+        console.log('All folders already exist.');
+    } else if (mkcount <= folders.length) {
+        console.log(mkcount + ' of ' + folders.length + ' folders were created.');
+    } else {
+        console.log('All folders successfully created.');
+    }
+  };
 
 function initializeApplication() {
     if(DEBUG) console.log('initializeApplication()');
@@ -11,9 +68,11 @@ function initializeApplication() {
         break;
     case '--cat':
         if(DEBUG) console.log('--cat createFiles()');
+        createFiles();
         break;
     case '--mk':
         if(DEBUG) console.log('--mk createFolders()');
+        createFolders();
         break;
     case '--help':
     case '--h':
